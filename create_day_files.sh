f9b3293809d50ce45957a88448dc21eafa66fc8c
Matt Kornfield <matt.kornfield@appian.com>
2020-12-17 00:39:02 -0500

    create script with different packages

--- create_day_files.sh
+++ create_day_files.sh
@@ -5,7 +5,8 @@ if [[ -z "$dayNumber" ]]; then
     echo "Day number is required"
     exit 1
 fi
-mkdir -p day${dayNumber}/src/aoc
-touch day${dayNumber}/src/aoc/day${dayNumber}.go
-touch day${dayNumber}/src/aoc/day${dayNumber}_test.go
-touch day${dayNumber}/src/aoc/day${dayNumber}_puzzle.txt
+dir_to_make=day${dayNumber}/src/aoc${dayNumber}
+mkdir -p ${dir_to_make}
+touch ${dir_to_make}/day${dayNumber}.go
+touch ${dir_to_make}/day${dayNumber}_test.go
+touch ${dir_to_make}/day${dayNumber}_puzzle.txt
